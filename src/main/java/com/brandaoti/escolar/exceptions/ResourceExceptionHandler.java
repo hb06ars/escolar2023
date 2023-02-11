bbf31cadfc9e5bd2a5b8713df12eb7ed13a6d6af
Henrique Brandão <hb06ars@hotmail.com>
2023-02-11 12:45:32 -0300

    Ajustes
    Ajustes

--- src/main/java/com/brandaoti/escolar/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/brandaoti/escolar/exceptions/ResourceExceptionHandler.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -14,13 +15,23 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
+	
+	@ExceptionHandler(AccessDeniedException.class)
+	public ResponseEntity<StandardError> objectnotFoundException(AccessDeniedException ex,
+			HttpServletRequest request) {
+
+		StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.NOT_FOUND.value(),
+				"Seu perfil não pode acessar essa página.", ex.getMessage(), request.getRequestURI());
+
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+	}
 
 	@ExceptionHandler(ObjectNotFoundException.class)
 	public ResponseEntity<StandardError> objectnotFoundException(ObjectNotFoundException ex,
 			HttpServletRequest request) {
 
 		StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.NOT_FOUND.value(),
-				"Object Not Found", ex.getMessage(), request.getRequestURI());
+				"Objeto não encontrado", ex.getMessage(), request.getRequestURI());
 
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
 	}
@@ -40,7 +51,7 @@ public class ResourceExceptionHandler {
 			HttpServletRequest request) {
 
 		ValidationError errors = new ValidationError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(), 
-				"Validation error", "Erro na validação dos campos", request.getRequestURI());
+				"Violação de dados", "Erro na validação dos campos.", request.getRequestURI());
 		
 		for(FieldError x : ex.getBindingResult().getFieldErrors()) {
 			errors.addError(x.getField(), x.getDefaultMessage());
