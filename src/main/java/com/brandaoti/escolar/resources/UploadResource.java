87c3396c03bcdf86d8ab74c3d8597d5911bf0733
Henrique Brandão <hb06ars@hotmail.com>
2023-02-22 19:44:07 -0300

    ok
    ok

--- src/main/java/com/brandaoti/escolar/resources/UploadResource.java
+++ src/main/java/com/brandaoti/escolar/resources/UploadResource.java
@@ -5,9 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -30,11 +30,11 @@ public class UploadResource {
 	@Autowired
 	private AlunoService alunoService;
 
-	@PostMapping(value = "/uploadalunos")
+	@PostMapping(value = "/uploadalunos" )
 	@PreAuthorize("hasAnyRole('ADMINISTRADOR', 'PROFESSOR', 'ALUNO')")
-	public ResponseEntity<String> uploadAlunos(@ModelAttribute MultipartFile file) throws Exception {
+	public ResponseEntity<String> uploadAlunos(@RequestParam("file")MultipartFile arquivo) throws Exception {
 		ProcessaExcel processaExcel = new ProcessaExcel();
-		List<Tabela> tabelas = processaExcel.uploadAlunos(file);
+		List<Tabela> tabelas = processaExcel.uploadAlunos(arquivo);
     	
 		return ResponseEntity.ok().body("Upload efetuado com sucesso.");
 	}
@@ -42,9 +42,9 @@ public class UploadResource {
 	
 	@PostMapping(value = "/uploadaulas")
 	@PreAuthorize("hasAnyRole('ADMINISTRADOR', 'PROFESSOR', 'ALUNO')")
-	public ResponseEntity<String> uploadAulas(@ModelAttribute MultipartFile file) throws Exception {
+	public ResponseEntity<String> uploadAulas(@RequestParam("file") MultipartFile arquivo) throws Exception {
 		ProcessaExcel processaExcel = new ProcessaExcel();
-		List<Tabela> tabelas = processaExcel.uploadAlunos(file);
+		List<Tabela> tabelas = processaExcel.uploadAlunos(arquivo);
     	
 		return ResponseEntity.ok().body("Upload efetuado com sucesso.");
 	}
