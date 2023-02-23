718c9554172c7263854d4a439b99a9511d8fbbdb
Henrique Brandão <hb06ars@hotmail.com>
2023-02-23 00:21:35 -0300

    ok
    ok

--- src/main/java/com/brandaoti/escolar/resources/UploadResource.java
+++ src/main/java/com/brandaoti/escolar/resources/UploadResource.java
@@ -1,5 +1,6 @@
 package com.brandaoti.escolar.resources;
 
+import java.time.LocalTime;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,11 +12,19 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.brandaoti.escolar.domain.Aula;
 import com.brandaoti.escolar.domain.Tabela;
 import com.brandaoti.escolar.domain.Usuario;
+import com.brandaoti.escolar.domain.enums.EnumPeriodo;
+import com.brandaoti.escolar.domain.enums.EnumSemana;
+import com.brandaoti.escolar.dtos.AulaDTO;
 import com.brandaoti.escolar.dtos.UsuarioDTO;
 import com.brandaoti.escolar.services.AlunoService;
+import com.brandaoti.escolar.services.AulaService;
+import com.brandaoti.escolar.services.DisciplinaService;
 import com.brandaoti.escolar.services.PerfilService;
+import com.brandaoti.escolar.services.ProfessorService;
+import com.brandaoti.escolar.services.TurmaService;
 import com.brandaoti.escolar.utils.ProcessaExcel;
 
 @RestController
@@ -27,6 +36,18 @@ public class UploadResource {
 	
 	@Autowired
 	private PerfilService perfilService;
+	
+	@Autowired
+	private AulaService aulaService;
+	
+	@Autowired
+	private DisciplinaService disciplinaService;
+	
+	@Autowired
+	private TurmaService turmaService;
+	
+	@Autowired
+	private ProfessorService professorService;
 
 	@PostMapping(value = "/uploadalunos" )
 	@PreAuthorize("hasAnyRole('ADMINISTRADOR', 'PROFESSOR', 'ALUNO')")
@@ -66,6 +87,39 @@ public class UploadResource {
 	public ResponseEntity<String> uploadAulas(@RequestParam("file") MultipartFile file) throws Exception {
 		ProcessaExcel processaExcel = new ProcessaExcel();
 		List<Tabela> tabelas = processaExcel.uploadAlunos(file);
+		Aula aula = new Aula();
+    	for(Tabela t : tabelas) {
+    		switch (t.getColuna()) {
+    			case 0 :
+    				aula = new Aula();
+    				aula.setDiaDaSemana(EnumSemana.valueOf(t.getConteudo().toUpperCase()));
+    				break;
+    			case 1 :
+    				aula.setPeriodo(EnumPeriodo.valueOf(t.getConteudo().toUpperCase()));
+    				break;
+    			case 2 :
+    				aula.setInicioAula(LocalTime.parse(t.getConteudo()));
+    				break;
+    			case 3 :
+    				aula.setFimAula(LocalTime.parse(t.getConteudo()));
+    				break;
+    			case 4 :
+    				aula.setDisciplina(disciplinaService.findNomeDisciplina(t.getConteudo().toUpperCase()));
+    				break;
+    			case 5 :
+    				aula.setTurma(turmaService.findBySerieTurma(
+    					Integer.parseInt(t.getConteudo().toUpperCase().split("ª")[0]), 
+    					t.getConteudo().toUpperCase().split("ª")[1].toUpperCase())
+    				);
+    				break;
+    			case 9 :
+    				aula.setProfessor(professorService.buscarIdProfessor(Integer.parseInt(t.getConteudo().split(" - ")[0])));
+    				aulaService.create(new AulaDTO(aula));
+    				break;
+    			default:
+    				break;
+    		}
+    	}
     	
 		return ResponseEntity.ok().body("Upload efetuado com sucesso.");
 	}
